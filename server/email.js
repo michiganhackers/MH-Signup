f530604e893e59b45e8cad50e26dfa1bf2e6b8ca
Otto Sipe <ottosipe@gmail.com>
2013-09-08 19:39:54 -0400

    skeleton commit

---
+++ server/email.js
@@ -0,0 +1,41 @@
+var nodemailer = require("nodemailer"),
+    fs         = require("fs"),
+    jade       = require("jade");
+
+// create reusable transport method (opens pool of SMTP connections)
+var smtpTransport = nodemailer.createTransport("SMTP", {
+    service: "Mandrill",
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    } // {user,pass}
+});
+
+exports.send = function sendMail(user, temp, cb) {
+    // send mail with defined transport object
+    if(user == null) return;
+    var template = jade.compile(fs.readFileSync(__dirname+'/email/' + temp, 'utf8'));
+    var html = template({
+        name: user.name,
+        email: user.email
+    });
+
+    var opts = {
+        from: "This Website <email@domain.com>", // sender address
+        to: user.name+" <"+user.email+">", // list of receivers
+        subject: "Testing...", // Subject line
+        html: html, // html body
+        generateTextFromHTML: true
+    }
+    console.log(opts);
+
+    // send the email
+    smtpTransport.sendMail(opts, function(error, response){
+        if(error){
+            cb(error);
+        } else {
+            var msg = "Message sent: " + response.message;
+            cb(msg);
+        }
+    });
+};
